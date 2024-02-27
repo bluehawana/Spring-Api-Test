7e0d00a82051677e5d1508b4b99ae5c9dc2481eb
Lars Strömberg <lars@dsve.se>
2024-02-27 15:35:36 +0000

    Add FlowerModelTests.java

---
+++ src/test/java/se/campusmolndal/flowers/model/FlowerModelTests.java
@@ -0,0 +1,20 @@
+package se.campusmolndal.flowers.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+import se.campusmolndal.flowers.model.Flower;
+
+public class FlowerModelTests {
+
+    @Test
+    public void testFlowerCreation() {
+        fail("Not implemented yet");
+    }
+
+    @Test
+    public void testFlowerSetters() {
+        fail("Not implemented yet");
+    }
+}
